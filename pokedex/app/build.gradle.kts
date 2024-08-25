4a3409be4a7e2199dee3fcbf98ef7c12f9164817
Lucas Dos Santos Francisco <76749210+LucasDSantosF@users.noreply.github.com>
2024-08-25 11:00:20 -0300

    feat: adjust versions config

--- pokedex/app/build.gradle.kts
+++ pokedex/app/build.gradle.kts
@@ -31,17 +31,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_19
+        targetCompatibility = JavaVersion.VERSION_19
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "19"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
